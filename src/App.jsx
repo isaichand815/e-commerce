4772d72bcd1a5d757a8f8727be36e2b705764afb
Ganesh <ganeshvasireddi456@gmail.com>
2025-03-31 20:46:58 +0530

    Added routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import BuyerRegistration from './buyer/buyerRegistration'
+import SellerRegistration from './seller/sellerRegistration'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { Login } from './login/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <BuyerRegistration></BuyerRegistration>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path='/login' element={<Login/>}/>
+          <Route path="/buyer-registration" element={<BuyerRegistration />} />
+          <Route path="/seller-registration" element={<SellerRegistration />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
